841af663dd1a6f19eb4ff9c8234297daf96c0c2b
dorshaul1 <dor.asti@gmail.com>
2021-05-30 17:52:40 +0300

    adding add to editor

--- src/cmps/EditorAdd/EditorAdd.jsx
+++ src/cmps/EditorAdd/EditorAdd.jsx
@@ -1,12 +1,18 @@
 
 
+import { useState } from 'react'
 import './EditorAdd.scss'
 
 export const EditorAdd = (props) => {
-
+const cmpTypes =['Header','Footer','Map','Gallery','Text','Cards','Video','Divider','Form']
     return (
-        <div>
-        <h1>Add</h1>
+        <div className="editorAdd flex justify-center wrap">
+            <div className="types-container flex justify-center wrap">
+
+            {cmpTypes.map((type,idx)=><div key={idx} className="type-btn">{type}</div>)}
+            </div>
+        {/* <h1>Add</h1> */}
+
         </div>
     )
 }
