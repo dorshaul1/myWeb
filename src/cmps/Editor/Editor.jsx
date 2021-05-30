221ee31ddca3ba67b4930d9c175f64e40bb16f19
dorshaul1 <dor.asti@gmail.com>
2021-05-30 17:55:36 +0300

    adding add to editor

--- src/cmps/Editor/Editor.jsx
+++ src/cmps/Editor/Editor.jsx
@@ -9,11 +9,11 @@ export const Editor = (props) => {
     const [isEdit, setIsEdit] = useState(false)
     return (
         <div className="editor">
-            <button onClick={()=>setIsEdit(false)}>Add</button>
-            <button onClick={()=>setIsEdit(true)}>Edit</button>
-            {isEdit ? <EditorEdit /> : <EditorAdd />}
-            {/* <section className="add"></section> */}
-            {/* <section className="edit"></section> */}
+            <button onClick={() => setIsEdit(false)}>Add</button>
+            <button onClick={() => setIsEdit(true)}>Edit</button>
+            <div className="editor-container">
+                {isEdit ? <EditorEdit /> : <EditorAdd />}
+            </div>
         </div>
     )
 }
